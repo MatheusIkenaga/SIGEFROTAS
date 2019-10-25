74bd699f5b4d9cfe5936fb5ffd88546d2910483d
matheus.ikenaga <matheus.ikenaga@hotmail.com>
2019-10-25 15:37:34 -0300

    primeiro commit

--- SIGEFROTAS/src/CadastroMotorista.java
+++ SIGEFROTAS/src/CadastroMotorista.java
@@ -287,6 +287,7 @@ public class CadastroMotorista extends javax.swing.JFrame {
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
+            @Override
             public void run() {
                 new CadastroMotorista().setVisible(true);
             }
