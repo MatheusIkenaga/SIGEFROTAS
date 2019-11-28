219f33d6968ed27ea0915b9c2cc67898f73209bb
danihml <45270388+danihml@users.noreply.github.com>
2019-11-28 02:13:57 -0300

    bases atualizadas

--- PIM/js/global.js
+++ PIM/js/global.js
@@ -1,7 +1,7 @@
 function popOpen(janela) {
-  if ($(janela).hasClass('open')) {
-      $(janela).toggleClass('open');
-  } else {
-      $(janela).addClass('open');
-}
+    if ($(janela).hasClass('open')) {
+        $(janela).toggleClass('open');
+    } else {
+        $(janela).addClass('open');
+    }
 }
